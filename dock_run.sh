80b302ab495d01c906ca9bddbaf5f5f7824074da
Miro-Astore <miro.astore@gmail.com>
2019-12-13 17:50:53 +1100

    fixed annoying automation shell behavior i should find a better fix for. atm i'm just making the prep_rec_vina aliases more explicit in the dock_run.sh file

--- dock_run.sh
+++ dock_run.sh
@@ -1,29 +1,31 @@
 dock_run () { 
 source $HOME/.bashrc
+alias prep_rec_vina="/home/miro/md/mgltools_1.5.6/bin/pythonsh  /home/miro/md/mgltools_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py"
+alias prep_lig_vina="/home/miro/md/mgltools_1.5.6/bin/pythonsh /home/miro/md/mgltools_1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_ligand4.py"
 
 mkdir /dev/shm/docking_temp/
 
+prep_rec_vina -r $1
+prep_lig_vina -l $3
+
 coor=$(vmd -dispdev text -startup /home/miro/python_scripts/dummy_vmdrc.tcl $1 -e /home/miro/python_scripts/origin.tcl 2> /dev/null | grep -E  ^-\|^[0-9]\|^{    )  
 coor=$(echo $coor | sed 's/[{}]//g')
+
 
 x=$(echo $coor | awk  '{print $1}')
 y=$(echo $coor | awk  '{print $2}')
 z=$(echo $coor | awk  '{print $3}')
 
-prep_rec_vina -r $1
-prep_lig_vina -l $3
 
 rec=$(echo $1 | sed "s/\.pdb//g")
 lig=$(echo $3 | sed "s/\.pdb//g")
 
 if [ -z $4 ]
 then
-    echo "yeah nah"
     vina --exhaustiveness 1000 --num_modes 100 --receptor $rec\.pdbqt --ligand $lig\.pdbqt --center_x $x --center_y $y --center_z $z --size_x 70 --size_y 70 --size_z 200 
 else 
 
 
-    echo "hi there"
     echo "mol new $rec" > /dev/shm/docking_temp/flex_sc.tcl
     echo "set sel [atomselect top \'$4\']" >> /dev/shm/docking_temp/flex_sc.tcl
     echo "\$sel writepdb flex_sc.pdb" >> /dev/shm/docking_temp/flex_sc.tcl
